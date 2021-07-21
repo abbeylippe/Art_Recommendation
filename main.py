40e64dc01654d2fa9e7c8d80ae3c276cefd45f00
Abbey Lippe <abbey.lippe@student.unimelb.edu.au>
2021-07-21 12:12:36 +1000

    program now includes impressionist artists

--- main.py
+++ main.py
@@ -9,67 +9,98 @@ from bs4 import BeautifulSoup
 import re
 import random
 import tkinter as tk
-from tkinter import PhotoImage, ttk
 
-def processNames(artist_list, names):
-    text = artist_list.text
-    # split the text according to newline to create an array of names
-    text = text.split("\n")
+from requests.api import post
 
-    # pattern to match up until an opening bracket to extract just the names
-    match = "^[^\(]+"
-    r1 = re.compile(match)
+impressionism = "impressionism"
+avantGarde = "avant-garde"
+postmodern = "postmodern"
 
-    for name in text:
-        # also removes commas
-        name = name.split(',')
-        x = r1.search(name[0])
-        # remove trailing white spaces
-        names.append(x.group().rstrip(" "))
+def processNames(artist_lists, names, option):
+    index = 0
+    for artist_list in artist_lists:
+
+        if (option == impressionism and index == 1): # the elements reside within a paragraph
+            text = []
+            artist_list = artist_list.find_all('a') # process to get them into list form
+            for artist in artist_list:
+                text.append(artist.text)
+
+
+        else: 
+            text = artist_list.text
+            # split the text according to newline to create an array of names
+            text = text.split("\n")
+
+        # pattern to match up until an opening bracket to extract just the names
+        match = "^[^\(]+"
+        r1 = re.compile(match)
+
+        for name in text:
+            if (name[0].isalpha() == False):
+                continue
+            # also removes commas
+            name = name.split(',')
+            x = r1.search(name[0])
+            # remove trailing white spaces
+            names.add(x.group().rstrip(" "))
+        
+        index+=1
 
 def handleSearchPress():
     genre = entry.get() # returns the text entered into the entry box
 
     if (genre == "avant-garde" or genre == "avant garde" or genre == "modernism" or genre == "modernist"):
-        option = "avant-garde"
+        option = avantGarde
         URL = "https://en.wikipedia.org/wiki/List_of_avant-garde_artists"
 
     elif (genre == 'postmodernism' or genre == "contemporary" or genre == "postmodern"):
-        option = "postmodern"
+        option = postmodern
         URL = "https://en.wikipedia.org/wiki/List_of_contemporary_artists"
+    
+    elif (genre == "impressionism" or genre == "impressionist"):
+        option = impressionism
+        URL = "https://en.wikipedia.org/wiki/Impressionism"
     
     else: # genre is unrecognised
         answer['text'] = "genre not recognised, try again"
         return
 
-
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    # id that the list is in 
+    # id that the names are in 
     content = soup.find(id="mw-content-text")
+    artist_lists = []
 
-    if (option == "avant-garde"):
+    if (option == avantGarde):
         # list is the second ul of the section
-        first2 = content.find_all("ul", limit=2)
-        artists = first2[1]
-    elif (option == "postmodern"):
+        containing = content.find_all("ul", limit=2)
+        artists = containing[1]
+        artist_lists.append(artists)
+    elif (option == postmodern):
         # 26 lists start with the 3rd list (split alphabetically)
-        first3 = content.find_all("ul", limit=28)
-        artists = first3[2:]
+        containing = content.find_all("ul", limit=28)
+        artists = containing[2:]
+        for artist in artists:
+            artist_lists.append(artist)
+    elif(option == impressionism):
+        # list is in the 3rd ul of the section
+        containing = content.find_all("ul", limit=3)
+        artists = containing[2] 
+        artist_lists.append(artists)
+        #print(artists)
+        womenHeader = soup.find(id="Women_Impressionists").find_parent('h2')
+        paragraphs = womenHeader.find_next_siblings('p')
+        artist_lists.append(paragraphs[1]) # names reside in the second paragraph
 
-    names = []
+    names = set() # ensure the same name isn't added twice
 
-    if (option == "avant-garde"):
-        processNames(artists, names)
+    processNames(artist_lists, names, option)
 
-    else:
-        for artist_list in artists:   
-            processNames(artist_list, names)
-
-
-    name = random.choice(names)
+    lst = list(names) # allow for random.choice to be called
+    name = random.choice(lst)
     answer["text"] = name
 
 
